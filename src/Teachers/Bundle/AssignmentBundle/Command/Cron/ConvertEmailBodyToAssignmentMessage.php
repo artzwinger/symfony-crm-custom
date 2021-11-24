8ad75b679da41ac694d78da860fe5184e52e41d3
Art Zwinger <artzwinger@gmail.com>
2021-11-24 17:43:04 +0300

    refactor converting email body to assignment command and add message threads support

--- src/Teachers/Bundle/AssignmentBundle/Command/Cron/ConvertEmailBodyToAssignmentMessage.php
+++ src/Teachers/Bundle/AssignmentBundle/Command/Cron/ConvertEmailBodyToAssignmentMessage.php
@@ -13,6 +13,7 @@ use Oro\Bundle\CronBundle\Command\CronCommandInterface;
 use Oro\Bundle\EmailBundle\Entity\Email;
 use Oro\Bundle\FeatureToggleBundle\Checker\FeatureChecker;
 use Oro\Bundle\ImapBundle\Entity\ImapEmail;
+use Oro\Bundle\UserBundle\Entity\User;
 use Predis\ClientInterface;
 use Soundasleep\Html2Text;
 use Soundasleep\Html2TextException;
@@ -24,6 +25,7 @@ use Symfony\Component\Lock\Store\RedisStore;
 use Symfony\Component\Lock\Store\SemaphoreStore;
 use Teachers\Bundle\AssignmentBundle\Entity\Assignment;
 use Teachers\Bundle\AssignmentBundle\Entity\AssignmentMessage;
+use Teachers\Bundle\AssignmentBundle\Entity\AssignmentMessageThread;
 
 /**
  * The CLI command to convert emails to assignment messages
@@ -58,11 +60,12 @@ class ConvertEmailBodyToAssignmentMessage extends Command implements CronCommand
      * @param ClientInterface $redisClient
      */
     public function __construct(
-        FeatureChecker $featureChecker,
-        EntityManager $entityManager,
+        FeatureChecker  $featureChecker,
+        EntityManager   $entityManager,
         ActivityManager $activityManager,
         ClientInterface $redisClient
-    ) {
+    )
+    {
         parent::__construct();
 
         $this->featureChecker = $featureChecker;
@@ -122,7 +125,7 @@ class ConvertEmailBodyToAssignmentMessage extends Command implements CronCommand
             /** @var Email $email */
             $email = $imapEmail->getEmail();
             try {
-                $this->processEmail($email);
+                $this->processEmail($email, $output);
             } catch (Exception $e) {
                 $output->writeln($e->getMessage());
             }
@@ -130,19 +133,106 @@ class ConvertEmailBodyToAssignmentMessage extends Command implements CronCommand
     }
 
     /**
-     * @throws OptimisticLockException
+     * @param Email $email
+     * @param OutputInterface $output
      * @throws ORMException
+     * @throws OptimisticLockException
      * @throws Exception
      */
-    protected function processEmail(Email $email)
+    protected function processEmail(Email $email, OutputInterface $output)
     {
-        $subject = $email->getSubject();
-        // find indicator in subject
-        if (!strstr($subject, 'New Message In The Assignment')) {
+        $this->validateEmailIsConvertible($email);
+        $assignment = $this->getAssignmentFromEmail($email);
+        $thread = $this->getThreadFromEmail($email, $output);
+
+        $assignmentMessage = new AssignmentMessage();
+        $assignmentMessage->setMessage($this->extractMessageTextFromEmail($email));
+        $assignmentMessage->setOrganization($assignment->getOrganization());
+
+        $sender = $this->getMessageSenderUser($email, $assignment);
+        $recipient = $this->getMessageRecipientUser($email, $assignment);
+
+        if (!$thread->getId()) {
+            $thread->setSender($sender);
+            $thread->setRecipient($recipient);
+            $this->entityManager->persist($thread);
+            $this->entityManager->flush($thread);
+        }
+
+        $assignmentMessage->setOwner($sender);
+        $assignmentMessage->setThread($thread);
+        $assignmentMessage->setRecipient($recipient);
+
+        $this->activityManager->addActivityTarget($assignmentMessage, $assignment);
+        $this->entityManager->persist($assignmentMessage);
+        $this->entityManager->flush($assignmentMessage);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function validateEmailIsConvertible(Email $email)
+    {
+        if (!strstr($email->getSubject(), 'New Message In The Assignment')) {
             throw new Exception('Subject of email #' . $email->getId() . ' does not contain assignment message indicators');
         }
-        // parse assignment id
-        preg_match('/#([0-9]+)/', $subject, $matches);
+    }
+
+    /**
+     * Returns a sender user if it's a tutor or a student assigned to the assignment
+     * Otherwise throws an exception
+     * @throws Exception
+     */
+    private function getMessageSenderUser(Email $email, Assignment $assignment): User
+    {
+        $sender = $email->getFromEmailAddress()->getEmail();
+        $teacher = $assignment->getTeacher();
+        $student = $assignment->getStudent();
+        if ($teacher->getEmail() === $sender) {
+            return $teacher;
+        }
+        if ($student->getEmail() === $sender) {
+            return $student;
+        }
+        throw new Exception('Cannot put message from email #' . $email->getId() . ' to assignment #' . $assignment->getId() . ': sender is not student or tutor');
+    }
+
+    /**
+     * Returns a recipient user if it's a tutor or a student assigned to the assignment
+     * Otherwise throws an exception
+     * @throws Exception
+     */
+    private function getMessageRecipientUser(Email $email, Assignment $assignment): User
+    {
+        $sender = $email->getFromEmailAddress()->getEmail();
+        $teacher = $assignment->getTeacher();
+        $student = $assignment->getStudent();
+        if ($teacher->getEmail() === $sender) {
+            return $student;
+        }
+        if ($student->getEmail() === $sender) {
+            return $teacher;
+        }
+        throw new Exception('Cannot put message from email #' . $email->getId() . ' to assignment #' . $assignment->getId() . ': not possible to guess a recipient');
+    }
+
+    private function extractMessageTextFromEmail(Email $email): string
+    {
+        try {
+            $body = $email->getEmailBody()->getBodyContent();
+            $body = $this->removeBlockQuote($body);
+            return trim(Html2Text::convert($body));
+        } catch (Html2TextException $e) {
+            return $email->getEmailBody()->getTextBody();
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getAssignmentFromEmail(Email $email): Assignment
+    {
+        preg_match('/Assignment #([0-9]+)/', $email->getSubject(), $matches);
         if (count($matches) < 2) {
             throw new Exception('Subject of email #' . $email->getId() . ' contains assignment message indicator, but does not contain assignment ID');
         }
@@ -152,8 +242,31 @@ class ConvertEmailBodyToAssignmentMessage extends Command implements CronCommand
         if (!$assignment) {
             throw new Exception('Cannot find assignment #' . $assignmentId . ' for email #' . $email->getId());
         }
-        $body = $email->getEmailBody()->getBodyContent();
+        return $assignment;
+    }
 
+    /**
+     * @throws Exception
+     */
+    private function getThreadFromEmail(Email $email, OutputInterface $output): AssignmentMessageThread
+    {
+        preg_match('/Thread #([0-9]+)/', $email->getSubject(), $matches);
+        if (count($matches) < 2) {
+            $output->writeLn('Subject of email #' . $email->getId() . ' does not contain thread identifier, creating a new thread...');
+            return new AssignmentMessageThread();
+        }
+        $threadId = $matches[1];
+        /** @var AssignmentMessageThread|null $thread */
+        $thread = $this->entityManager->getRepository(AssignmentMessageThread::class)->find($threadId);
+        if (!$thread) {
+            $output->writeLn('Cannot find thread #' . $threadId . ' for email #' . $email->getId() . ', creating a new thread...');
+            return new AssignmentMessageThread();
+        }
+        return $thread;
+    }
+
+    private function removeBlockQuote(string $body)
+    {
         $doc = new DOMDocument();
         $doc->loadHTML($body);
 
@@ -177,30 +290,6 @@ class ConvertEmailBodyToAssignmentMessage extends Command implements CronCommand
                 $quote->parentNode->removeChild($quote);
             }
         }
-
-        $body = $doc->saveHTML();
-        $assignmentMessage = new AssignmentMessage();
-        try {
-            $assignmentMessage->setMessage(trim(Html2Text::convert($body)));
-        } catch (Html2TextException $e) {
-            $assignmentMessage->setMessage($email->getEmailBody()->getTextBody());
-        }
-
-        $assignmentMessage->setOrganization($assignment->getOrganization());
-        // guess who is sender and make sender a message owner
-        $sender = $email->getFromEmailAddress()->getEmail();
-        $teacher = $assignment->getTeacher();
-        $student = $assignment->getStudent();
-        if ($teacher->getEmail() === $sender) {
-            $assignmentMessage->setOwner($teacher);
-        } else if ($student->getEmail() === $sender) {
-            $assignmentMessage->setOwner($student);
-        } else {
-            throw new Exception('Cannot put message from email #' . $email->getId() . ' to assignment #' . $assignmentId . ': sender is not student or tutor');
-        }
-        // save and attach activity target
-        $this->activityManager->addActivityTarget($assignmentMessage, $assignment);
-        $this->entityManager->persist($assignmentMessage);
-        $this->entityManager->flush($assignmentMessage);
+        return $doc->saveHTML();
     }
 }
