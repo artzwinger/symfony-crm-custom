b4908bc3ade471e1637318209a1ac582e1405238
Art Zwinger <artzwinger@gmail.com>
2021-11-25 18:18:38 +0300

    remove unread_message_flash kernel listener and add websocket channel and pusher

---
+++ src/Teachers/Bundle/AssignmentBundle/EventListener/ORM/NotifyUserNewMessage.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Teachers\Bundle\AssignmentBundle\EventListener\ORM;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Exception;
+use Oro\Bundle\SyncBundle\Client\WebsocketClientInterface;
+use Oro\Bundle\UserBundle\Entity\User;
+use Teachers\Bundle\AssignmentBundle\Entity\AssignmentMessage;
+use Teachers\Bundle\UsersBundle\Helper\Role;
+
+class NotifyUserNewMessage
+{
+    /**
+     * @var WebsocketClientInterface
+     */
+    private $websocketClient;
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+    /**
+     * @var Role
+     */
+    private $roleHelper;
+
+    /**
+     * @param EntityManager $entityManager
+     * @param Role $roleHelper
+     * @param WebsocketClientInterface $websocketClient
+     */
+    public function __construct(
+        EntityManager $entityManager,
+        Role $roleHelper,
+        WebsocketClientInterface $websocketClient
+    )
+    {
+        $this->entityManager = $entityManager;
+        $this->websocketClient = $websocketClient;
+        $this->roleHelper = $roleHelper;
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        /** @var AssignmentMessage $message */
+        $message = $args->getObject();
+        if (!$message instanceof AssignmentMessage) {
+            return;
+        }
+        $recipient = $message->getRecipient();
+        $recipientIds = $recipient ? $recipient->getId() : $this->getCourseManagerOrAdminUserIds();
+        $this->notifyRecipients($recipientIds);
+    }
+
+    private function getCourseManagerOrAdminUserIds(): array
+    {
+        $roles = [
+            $this->roleHelper->getAdminRole(),
+            $this->roleHelper->getCourseManagerRole()
+        ];
+        $users = $this->entityManager->getRepository(User::class)
+            ->findBy(['roles' => $roles]);
+        return array_map(function (User $user) {
+            return $user->getId();
+        }, $users);
+    }
+
+    private function notifyRecipients(array $ids)
+    {
+        foreach ($ids as $id) {
+            $this->websocketClient->publish('teachers/new_message/' . $id, '');
+        }
+    }
+}
